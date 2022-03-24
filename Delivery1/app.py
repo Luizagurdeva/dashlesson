3141219a137b88f656a8bbfcb5864c762817a3d3
Luiza <luiz0160@stud.kea.dk>
2022-03-24 11:26:28 +0100

    div

---
+++ Delivery1/app.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go 
+
+import dash
+from dash import html
+from dash import dcc
+from dash.dependencies import Input, Output
+
+app = dash.
