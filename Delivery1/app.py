237eaf0006ce03da3b70ea919a2b23956c1a908f
Luiza <luiz0160@stud.kea.dk>
2022-03-24 12:04:33 +0100

    div

--- Delivery1/app.py
+++ Delivery1/app.py
@@ -7,4 +7,10 @@ from dash import html
 from dash import dcc
 from dash.dependencies import Input, Output
 
-app = dash.
+import numpy as np
+import calendar
+
+import structured_data
+order = structured_data.get_data()
+df_year = structured_data.get_year()
+df_month = structured_data.get_month()
